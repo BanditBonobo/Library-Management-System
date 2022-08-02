73c0d2a800f4b38d79eba2047e8b88ed82076870
Haseeb Yousuf <m.haseebyousuf@gmail.com>
2022-08-03 00:48:38 +0400

    connection made to mysql database made

--- src/Project/ConnectionProvider.java
+++ src/Project/ConnectionProvider.java
@@ -3,11 +3,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Project;
+import java.sql.*;
 
 /**
  *
  * @author User
  */
 public class ConnectionProvider {
-    
+    public static Connection getCon(){
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/lms","root","Wq7Uxkj9CoJ346.");
+            return con;
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+            return null;
+        }
+    }
 }
